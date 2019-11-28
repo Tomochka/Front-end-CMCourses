977abd0d01438d650d043742f77c9e2e3e3c472f
tamara_lapina <darksta13@mail.ru>
2019-11-29 00:27:55 +0300

    Added refactoring sample.js

---
+++ Homework#4-JS/sampleBeforeCompile.js
@@ -0,0 +1,93 @@
+/**
+ Заносим игроков. Организуем турнирную сетку в два круга между игроками
+ Выводим результаты в консоль.
+ Запуск через node sample.js
+ */
+var players = [
+    'Alexey',
+    'Artur',
+    'Kirill',
+    'Vlad'
+]
+
+'using strict';//должен поправить????
+
+class Game {
+    constructor({ p1, p2 }) {
+          
+        this.player1 = p1;
+        this.player2 = p2;//должен поправить пробел
+      
+        this.isPlayer1Win = undefined;
+    }
+
+    play() {
+        var player1Win = Boolean(Math.round(Math.random()));//должен поставить пробел
+        switch (player1Win) {
+            case true:
+                this.isPlayer1Win = true;
+                console.info(this.player1, '1 - 0', this.player2);
+                break;
+            case false:
+                    this.isPlayer1Win = false;
+                    console.info(this.player1, '0 - 1', this.player2);
+                    break;
+        }
+    }
+}
+
+class Championship {
+    constructor(gamers) {
+        this.currentGame = [];
+        this.allGames = [];
+        this.gamers = gamers;
+    }
+
+    setSchedule() {
+
+        this.currentGame = [];
+
+        for (var i = 0; i < this.gamers.length; i++) {
+            for (var j = 0; j < this.gamers.length; j++) {
+                if (i < j) this.currentGame.push(new Game({ p1: this.gamers[i], p2: this.gamers[j] }));
+            }
+        }
+
+        this.allGames.push(...this.currentGame);
+    }
+
+    play() {
+        this.currentGame.map((game) => game.play());
+    }
+
+    getResults() {
+        return this.allGames.reduce((prev, current) => {
+            if (current.isPlayer1Win) {
+                return {
+                    ...prev,
+                    [current.player1]: (prev[current.player1] || 0) + 1,
+                    [current.player2]: (prev[current.player2] || 0)
+                }
+            }
+
+            return {
+                ...prev,
+                [current.player1]: (prev[current.player1] || 0),
+                [current.player2]: (prev[current.player2] || 0) + 1
+            }
+        },{})
+    }
+}
+
+
+const champinonship = new Championship(players);
+const games = 2;
+
+for (let i=0; i<games; i++){
+    console.info(`${i+1}nd round`);
+    champinonship.setSchedule();
+    champinonship.play(); 
+}
+
+let results = champinonship.getResults();
+console.info('Results', results);
